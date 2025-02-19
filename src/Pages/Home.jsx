46451203c3a87e784fcced1ee251a1cd28887446
Nadjimfen <148588237+Nadjimfen@users.noreply.github.com>
2025-02-19 15:51:13 +0100

    Update Home.jsx

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -2,36 +2,34 @@ import React from "react";
 import Particles from "react-tsparticles";
 import { loadSlim } from "tsparticles-slim";
 import { useNavigate } from "react-router-dom";
+
 function Home() {
     const particlesInit = async (engine) => {
         await loadSlim(engine);
-      };
-      
-      const navigate = useNavigate();
+    };
+    
+    const navigate = useNavigate();
 
-  return (
-    <div className="home">
-      <Particles
-        id="particles-js"
-        init={particlesInit}
-        options={{
-          particles: {
-            number: { value: 100 },
-            size: { value: 3 },
-            move: { speed: 1 }
-          }
-        }}
-      />
-      <div className="home-content">
-        {/* <img src="/stillife.jpg" alt="Profil" className="profile-image" /> */}
-        <h1>Hi, I'm <span className="highlight">Fennouch Nadjim</span></h1>
-        <h2>Web Developer</h2>
-        <p>Passionné par le développement web moderne et interactif.</p>
-            
-
-        <button onClick={()=>navigate("/Contact")}className="btn">Me Contacter</button>
-      </div>
-      <div className="technologies">
+    return (
+        <div className="home">
+            <Particles
+                id="particles-js"
+                init={particlesInit}
+                options={{
+                    particles: {
+                        number: { value: 100 },
+                        size: { value: 3 },
+                        move: { speed: 1 }
+                    }
+                }}
+            />
+            <div className="home-content">
+                <h1>Hi, I'm <span className="highlight">Fennouch Nadjim</span></h1>
+                <h2>Web Developer</h2>
+                <p>Passionné par le développement web moderne et interactif.</p>
+                <button onClick={() => navigate("/Contact")} className="btn">Me Contacter</button>
+            </div>
+            <div className="technologies">
                 <h2>Technologies que je maîtrise</h2>
                 <ul>
                     <li>Laravel</li>
@@ -42,8 +40,6 @@ function Home() {
                     <li>Java</li>
                 </ul>
             </div>
-
-            {/* Section IDEs */}
             <div className="ides">
                 <h2>Environnements de développement (IDE)</h2>
                 <ul>
@@ -54,8 +50,8 @@ function Home() {
                     <li>Wireshark</li>
                 </ul>
             </div>
-    </div>
-  );
+        </div>
+    );
 }
 
 export default Home;
